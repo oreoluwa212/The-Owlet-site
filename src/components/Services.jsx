8efad8dffbc969e629d13f468521c2c4cdf922f6
Oreoluwa Ajayi <oreoluwaajayyiruth@gmail.com>
2024-05-15 12:12:48 -0700

    fix

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,9 +3,9 @@ import { facebook, instagram, soundCloud, tiktok, vimeo, x, youtube } from "../a
 function Services() {
    return (
       <div className="lgss:px-[20%] -mt-12">
-      <div className="bg-white py-3 rounded-[32px] border shadow-lg flex flex-col gap-3 justify-between items-center px-5">
-         <h1 className="uppercase text-secondary font-semibold">Our services:</h1>
-         <div className="flex justify-between">
+      <div className="bg-white py-3 rounded-[32px] border shadow-lg flex lgss:flex-row flex-col gap-3 justify-between items-center px-5 w-full">
+         <h1 className="uppercase text-secondary font-semibold text-[18px]">Our services:</h1>
+         <div className="flex justify-between w-[80%]">
             <img src={instagram} alt="" />
             <img src={tiktok} alt="" />
             <img src={x} alt="" />
