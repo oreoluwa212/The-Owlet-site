6ada2442ce947b3b22171c21ac732bc255bb0e7b
oreoluwa ajayi <oreoluwaajayyiruth@gmail.com>
2024-05-17 08:16:19 +0100

    fix: testing overflow

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -4,9 +4,9 @@ import Card from "../components/onboarding/Card";
 
 const SignUpPage = () => {
   return (
-    <div className="bg-[rgb(249,250,251)] pb-10">
+    <div className="bg-[rgb(249,250,251)] overflow-hidden h-screen">
       <BoardingNavMenu />
-      <div className="w-full flex justify-center lgss:pt-14 pt-4 h-full">
+      <div className="w-full flex justify-center lgss:pt-10 pt-4">
         <Card h1="Create an account" p="Sign up in less than 2 minutes." />
       </div>
     </div>
